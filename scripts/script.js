fd078b5c8397e5463d81456be79482ac875428dd
peterihimire <peterihimire@gmail.com>
2019-12-17 13:59:48 +0100

    Updates

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+const navbarBtn = document.querySelector('.navbar_btn');
+const navbarLinks = document.querySelector('.navbar_links');
+
+// For menu button
+navbarBtn.addEventListener('click' , () => {
+    let value = navbarLinks.classList.contains('navbar_collapse');
+
+    if(value){
+        navbarLinks.classList.remove('navbar_collapse');
+        navbarBtn.classList.remove('change');
+    }else{
+        navbarLinks.classList.add('navbar_collapse');
+        navbarBtn.classList.add('change');
+    }
+})
+
+// For Glider JS 
+// new Glider(document.querySelector('.glider'), {
+//     slidesToShow: 4,
+//     slidesToScroll: 1,
+//     draggable: true,
+//     duration: 0.5,
+//     dots: '#dots',
+//     arrows: {
+//         prev: '.glider-prev',
+//         next: '.glider-next'
+//     }
+// });
+
+// For AOS
+AOS.init();
