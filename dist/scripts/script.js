d5f6cc8028e9b4ad5c6d0a79671d69d5aa88613b
peterihimire <peterihimire@gmail.com>
2019-12-30 01:08:29 +0100

    Added some validation script to work with my contact form

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,6 +1,17 @@
 const navbarBtn = document.querySelector('.navbar_btn');
 const navbarLinks = document.querySelector('.navbar_links');
 
+
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const messageInput = document.querySelector('#message');
+const button = document.querySelector('#c-button');
+const msgAlert = document.querySelector('#msg');
+const closeBtn = document.querySelector('#close')
+
+
+
+
 // For menu button
 navbarBtn.addEventListener('click' , () => {
     let value = navbarLinks.classList.contains('navbar_collapse');
@@ -14,18 +25,32 @@ navbarBtn.addEventListener('click' , () => {
     }
 })
 
-// For Glider JS 
-// new Glider(document.querySelector('.glider'), {
-//     slidesToShow: 4,
-//     slidesToScroll: 1,
-//     draggable: true,
-//     duration: 0.5,
-//     dots: '#dots',
-//     arrows: {
-//         prev: '.glider-prev',
-//         next: '.glider-next'
-//     }
-// });
 
 // For AOS
 AOS.init();
+
+
+// For The Contact Form Validation
+button.addEventListener('click', (e)=> {
+  e.preventDefault()
+  console.log('Button has been clicked.');
+  console.log(e.target)
+
+  if(nameInput.value === '' || emailInput.value === '' || messageInput.value === ''){
+   
+    msgAlert.innerHTML = `
+    <p>Please enter all fields.</p>
+    <span id="close" class="closebtn">&times</span>
+    `;
+    msgAlert.classList.add('error');
+
+
+    setTimeout( ()=> {
+      msgAlert.remove();
+    }, 3000)
+   
+  } else {
+    console.log('Success')
+  }
+})
+
