a93c2c8953581dba846138c2561736937c35d6b6
peterihimire <peterihimire@gmail.com>
2020-01-17 22:37:00 +0100

    made the links to open on a new tab via target="_blank", also made it a little more mobile friendly by reducing the fonts for the lg and sm heading, then I also worked on the contact validation , for now yet to work on the linkedin link

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -2,6 +2,7 @@ const navbarBtn = document.querySelector('.navbar_btn');
 const navbarLinks = document.querySelector('.navbar_links');
 
 
+const contactForm = document.querySelector('#contact-form')
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
 const messageInput = document.querySelector('#message');
@@ -31,26 +32,53 @@ AOS.init();
 
 
 // For The Contact Form Validation
-button.addEventListener('click', (e)=> {
-  e.preventDefault()
+
+
+const onSubmit = (event) => {
+  event.preventDefault()
   console.log('Button has been clicked.');
-  console.log(e.target)
+  console.log(event.target)
 
   if(nameInput.value === '' || emailInput.value === '' || messageInput.value === ''){
    
+    msgAlert.classList.add('error');
     msgAlert.innerHTML = `
     <p>Please enter all fields.</p>
     <span id="close" class="closebtn">&times</span>
     `;
-    msgAlert.classList.add('error');
-
-
-    setTimeout( ()=> {
-      msgAlert.remove();
-    }, 3000)
+   
+    setTimeout( () => msgAlert.remove(), 3000);
    
   } else {
     console.log('Success')
   }
-})
+
+}
+
+contactForm.addEventListener('submit', onSubmit);
+
+
+// contactForm.addEventListener('submit', (e)=> {
+//   e.preventDefault()
+//   console.log('Button has been clicked.');
+//   console.log(e.target)
+
+//   if(nameInput.value === '' || emailInput.value === '' || messageInput.value === ''){
+   
+//     msgAlert.classList.add('error');
+//     msgAlert.innerHTML = `
+//     <p>Please enter all fields.</p>
+//     <span id="close" class="closebtn">&times</span>
+//     `;
+   
+
+
+//     setTimeout( ()=> {
+//       msgAlert.remove();
+//     }, 3000)
+   
+//   } else {
+//     console.log('Success')
+//   }
+// })
 
