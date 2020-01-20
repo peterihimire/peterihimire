6c93b1dbf194c6fbe9f0aa3110ea5a2e274bb36a
peterihimire <peterihimire@gmail.com>
2020-01-20 04:23:50 +0100

    Updates

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ const navbarBtn = document.querySelector('.navbar_btn');
 const navbarLinks = document.querySelector('.navbar_links');
 
 
+const contactForm = document.querySelector('#contact-form')
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
 const messageInput = document.querySelector('#message');
@@ -14,43 +15,58 @@ const closeBtn = document.querySelector('#close')
 
 // For menu button
 navbarBtn.addEventListener('click' , () => {
-    let value = navbarLinks.classList.contains('navbar_collapse');
+  let value = navbarLinks.classList.contains('navbar_collapse');
 
-    if(value){
-        navbarLinks.classList.remove('navbar_collapse');
-        navbarBtn.classList.remove('change');
-    }else{
-        navbarLinks.classList.add('navbar_collapse');
-        navbarBtn.classList.add('change');
-    }
+  if(value){
+    navbarLinks.classList.remove('navbar_collapse');
+    navbarBtn.classList.remove('change');
+  }else{
+    navbarLinks.classList.add('navbar_collapse');
+    navbarBtn.classList.add('change');
+  }
 })
+
 
 
 // For AOS
 AOS.init();
 
 
+
 // For The Contact Form Validation
-button.addEventListener('click', (e)=> {
-  e.preventDefault()
+const onSubmit = (event) => {
+  event.preventDefault()
   console.log('Button has been clicked.');
-  console.log(e.target)
+  console.log(event.target)
 
   if(nameInput.value === '' || emailInput.value === '' || messageInput.value === ''){
    
-    msgAlert.innerHTML = `
-    <p>Please enter all fields.</p>
-    <span id="close" class="closebtn">&times</span>
-    `;
     msgAlert.classList.add('error');
-
-
-    setTimeout( ()=> {
-      msgAlert.remove();
-    }, 3000)
+    msgAlert.innerHTML = `
+      <p>Please enter all fields.</p>
+      <span id="close" class="closebtn">&times</span>
+    `;
+   
+    setTimeout( () => msgAlert.remove(), 3000);
    
   } else {
     console.log('Success')
+    msgAlert.classList.add('success');
+    msgAlert.innerHTML = `
+      <p>Thank you! Your form submission has been received.</p>
+      <span id="close" class="closebtn">&times</span>
+    `;
+   
+    setTimeout( () => msgAlert.remove(), 3000);
+    nameInput.value = '';
+    emailInput.value = '';
+    messageInput.value = '';
   }
-})
+
+}
+
+contactForm.addEventListener('submit', onSubmit);
+
+
+
 
