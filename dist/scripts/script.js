7aae48f29b20fd687b3219bc06e11e6b55c083e4
peterihimire <peterihimire@gmail.com>
2020-01-20 04:17:12 +0100

    Updatad the tablet and PC navbar to be wider in terms of the height via padding, reduced the lg and sm-heading to be mobile friendly, added a success validation info, added corresponding color to the skill hr

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -15,25 +15,25 @@ const closeBtn = document.querySelector('#close')
 
 // For menu button
 navbarBtn.addEventListener('click' , () => {
-    let value = navbarLinks.classList.contains('navbar_collapse');
+  let value = navbarLinks.classList.contains('navbar_collapse');
 
-    if(value){
-        navbarLinks.classList.remove('navbar_collapse');
-        navbarBtn.classList.remove('change');
-    }else{
-        navbarLinks.classList.add('navbar_collapse');
-        navbarBtn.classList.add('change');
-    }
+  if(value){
+    navbarLinks.classList.remove('navbar_collapse');
+    navbarBtn.classList.remove('change');
+  }else{
+    navbarLinks.classList.add('navbar_collapse');
+    navbarBtn.classList.add('change');
+  }
 })
+
 
 
 // For AOS
 AOS.init();
 
 
+
 // For The Contact Form Validation
-
-
 const onSubmit = (event) => {
   event.preventDefault()
   console.log('Button has been clicked.');
@@ -43,14 +43,24 @@ const onSubmit = (event) => {
    
     msgAlert.classList.add('error');
     msgAlert.innerHTML = `
-    <p>Please enter all fields.</p>
-    <span id="close" class="closebtn">&times</span>
+      <p>Please enter all fields.</p>
+      <span id="close" class="closebtn">&times</span>
     `;
    
     setTimeout( () => msgAlert.remove(), 3000);
    
   } else {
     console.log('Success')
+    msgAlert.classList.add('success');
+    msgAlert.innerHTML = `
+      <p>Thank you! Your form submission has been received.</p>
+      <span id="close" class="closebtn">&times</span>
+    `;
+   
+    setTimeout( () => msgAlert.remove(), 3000);
+    nameInput.value = '';
+    emailInput.value = '';
+    messageInput.value = '';
   }
 
 }
@@ -58,27 +68,5 @@ const onSubmit = (event) => {
 contactForm.addEventListener('submit', onSubmit);
 
 
-// contactForm.addEventListener('submit', (e)=> {
-//   e.preventDefault()
-//   console.log('Button has been clicked.');
-//   console.log(e.target)
 
-//   if(nameInput.value === '' || emailInput.value === '' || messageInput.value === ''){
-   
-//     msgAlert.classList.add('error');
-//     msgAlert.innerHTML = `
-//     <p>Please enter all fields.</p>
-//     <span id="close" class="closebtn">&times</span>
-//     `;
-   
-
-
-//     setTimeout( ()=> {
-//       msgAlert.remove();
-//     }, 3000)
-   
-//   } else {
-//     console.log('Success')
-//   }
-// })
 
